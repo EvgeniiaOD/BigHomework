3619721647292dd41a81de9bf115c8576b8e457a
Evgeniia <evgeniia.d@gmail.com>
2023-06-19 22:41:38 +0200

    added code with existed array

--- June2023/Program.cs
+++ June2023/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿// Решение с заранее заданным массивом:
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+int ArrayLengthCount(string[] arr , int limit)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= limit && arr[i].Length != 0) count++;
+    }
+    return count;
+}
+string[] ResultArray(string[] arr, int count, int limit)
+{
+    string[] resArr = new string[count];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= limit && arr[i].Length != 0)
+        {
+            resArr[j] = arr[i];
+            j++;
+        }
+    }
+    return resArr;
+}
+
+string valOne = "One";
+string valTwo = "Two";
+string valThree = "Three";
+int characterLimit = 3;
+string[] initialArray = { valOne, valTwo, valThree };
+Console.Write("[");
+PrintArray(initialArray);
+Console.Write("] -> [");
+int newArrLength = ArrayLengthCount(initialArray, characterLimit);
+string[] resultArray = ResultArray(initialArray, newArrLength, characterLimit);
+PrintArray(resultArray);
+Console.Write("]");
